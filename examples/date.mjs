01b5f4fce49f4ed1284bc03fa4bc6141173959ba
pcanz <cashin.peter@gmail.com>
2022-01-05 16:15:26 -0500

    minor add

--- examples/date.mjs
+++ examples/date.mjs
@@ -14,3 +14,15 @@ const p = dg.parse("2021-4-05  xxx");
 
 if (p.ok) console.log(JSON.stringify(p.ptree));
 else console.log(p.err);
+
+const dt = peg.compile(`
+    Date  = year '-' month '-' day
+    year  = [0-9]*4
+    month = [0-9]*1.. 
+    day   = [0-9]*1..2
+`)
+
+const d = dt.parse("2021-04-0567");
+
+if (d.ok) console.log(JSON.stringify(d.ptree));
+else console.log(d.err);
